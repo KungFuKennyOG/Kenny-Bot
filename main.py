af1d74f2b9326ba464d667c875d88a6889fb619c
KungFuKennyOG <79944172+KungFuKennyOG@users.noreply.github.com>
2022-02-20 00:47:13 -0500

    Added a few commands for the future music feature

--- main.py
+++ main.py
@@ -2,53 +2,103 @@ import discord
 import os
 
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 
-client = discord.Client()
+client = commands.Bot(command_prefix='$', help_command=None)
 
 
+# GIFs commands
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def drunk(ctx):
+    await ctx.send('https://tenor.com/view/drunk-gif-22301749')
 
-    if message.content.startswith('$drunk'):
-        await message.channel.send('https://tenor.com/view/drunk-gif-22301749')
 
-    if message.content.startswith('$slap'):
-        await message.channel.send('https://tenor.com/view/slapping-mad-rage-anime-backhand-gif-16422437')
+@client.command()
+async def slap(ctx):
+    await ctx.send('https://tenor.com/view/slapping-mad-rage-anime-backhand-gif-16422437')
 
-    if message.content.startswith('$wakeup'):
-        await message.channel.send('https://tenor.com/view/sleep-wake-up-wake-up-in-night-ren-and-stimpy-gif-24104879')
 
-    if message.content.startswith('$depressed'):
-        await message.channel.send('https://tenor.com/view/sad-raining-gif-17973911')
+@client.command()
+async def wakeup(ctx):
+    await ctx.send('https://tenor.com/view/sleep-wake-up-wake-up-in-night-ren-and-stimpy-gif-24104879')
 
-    if message.content.startswith('$boom'):
-        await message.channel.send('https://tenor.com/view/penis-firework-explosion-boom-gif-4932263')
 
-    if message.content.startswith('$pray'):
-        await message.channel.send('https://tenor.com/view/halal-pray-praying-cat-praying-cat-gif-19802221')
+@client.command()
+async def depressed(ctx):
+    await ctx.send('https://tenor.com/view/sad-raining-gif-17973911')
 
-    if message.content.startswith('$dance'):
-        await message.channel.send('https://media.discordapp.net/attachments/741858155075469393/894749228222541885/koala-fab-02.gif')
 
-    if message.content.startswith('$vape'):
-        await message.channel.send('https://media.discordapp.net/attachments/753289564344549519/908183938940436490/received_583865662581686.gif')
+@client.command()
+async def boom(ctx):
+    await ctx.send('https://tenor.com/view/penis-firework-explosion-boom-gif-4932263')
 
-    if message.content.startswith('$help'):
+
+@client.command()
+async def pray(ctx):
+    await ctx.send('https://tenor.com/view/halal-pray-praying-cat-praying-cat-gif-19802221')
+
+
+@client.command()
+async def dance(ctx):
+    await ctx.send('https://media.discordapp.net/attachments/741858155075469393/894749228222541885/koala-fab-02.gif')
+
+
+@client.command()
+async def vape(ctx):
+    await ctx.send(
+        'https://media.discordapp.net/attachments/753289564344549519/908183938940436490/received_583865662581686.gif')
+
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(
+        title='Possible Commands',
+        description="$drunk, $slap, $wakeup, $depressed, $boom, $pray, $dance, $vape",
+        color=1146986
+    )
+    await ctx.send(embed=embed)
+
+
+# Music commands
+
+@client.command()
+async def play(ctx):
+    if not ctx.message.author.voice:
         embed = discord.Embed(
-            title='Possible Commands',
-            description="$drunk, $slap, $wakeup, $depressed, $boom, $pray, $dance, $vape",
-            color=1146986
+            description="You need to be in a voice channel to run this command!!"
         )
+        await ctx.send(embed=embed)
+    elif ctx.voice_client:
+        embed = discord.Embed(
+            description="I am already in a voice channel, you dummy >:("
+        )
+        await ctx.send(embed=embed)
+    else:
+        voice_channel = ctx.message.author.voice.channel
+        await voice_channel.connect()
 
-        await message.channel.send(embed=embed)
+
+@client.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if ctx.voice_client:
+        embed = discord.Embed(
+            description="I left the voice channel"
+        )
+        await ctx.send(embed=embed)
+        await ctx.guild.voice_client.disconnect()
+    else:
+        embed = discord.Embed(
+            description="I am already not in a voice channel :/"
+        )
+        await ctx.send(embed=embed)
+
 
 client.run(os.getenv('TOKEN'))
